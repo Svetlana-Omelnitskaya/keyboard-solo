7c13accb3c939e4cb9b8ba2783565e15427c8e15
Svetlana-Omelnitskaya <omelnitskayas@list.ru>
2025-05-29 20:33:41 +0300

    Домашнее задание - 11

---
+++ script.js
@@ -0,0 +1,82 @@
+const words = ["apple", "sofa", "notes", "banana", "pencil", "date", "table", "froge", "laptop"];
+let currentWord = "";
+let currentElementIndex = 0;
+let correctCount = 0;
+let wrongCount = 0;
+let currentWordMistakes = 0;
+
+function setRandomWord() {
+    currentWord = words[Math.floor(Math.random() * words.length)];
+    currentElementIndex = 0;
+    currentWordMistakes = 0;
+    showWord();
+}
+
+function showWord() {
+    const wordContainer = document.querySelector('.word');
+    wordContainer.innerHTML = '';
+    currentWord.split('').forEach((element, index) => {
+        const span = document.createElement('span');
+        span.textContent = element;
+        span.classList.add('element');
+        if (index < currentElementIndex) {
+            span.classList.add('c');
+        }
+        wordContainer.append(span);
+    });
+}
+
+function handleInput(event) {
+    const inputElement = event.key;
+
+    if (inputElement === currentWord[currentElementIndex]) {
+        const wordContainer = document.querySelector('.word');
+        const currentSpan = wordContainer.children[currentElementIndex];
+        currentSpan.classList.add('c');
+        currentElementIndex++;
+        
+        if (currentElementIndex === currentWord.length) {
+            correctCount++;
+            if (correctCount === 5) {
+                alert("Поздравляем! Вы выиграли!");
+                resetGame();
+            } else {
+                setRandomWord();
+            }
+        }
+        
+    } else {
+        const wordContainer = document.querySelector('.word');
+        const currentSpan = wordContainer.children[currentElementIndex];
+        currentSpan.classList.add('w');
+        wrongCount++;
+        currentWordMistakes++;
+        updateStatus();
+        
+        if (wrongCount === 5) {
+            alert("К сожалению, вы проиграли!");
+            resetGame();
+            return;
+        }
+    }
+
+    updateStatus();
+    showWord();
+}
+
+function updateStatus() {
+    document.querySelector('.correct-count').textContent = correctCount;
+    document.querySelector('.wrong-count').textContent = wrongCount;
+    document.querySelector('.word-mistakes').textContent = currentWordMistakes;
+}
+
+function resetGame() {
+    correctCount = 0;
+    wrongCount = 0;
+    currentElementIndex = 0;
+    updateStatus();
+    setRandomWord();
+}
+
+setRandomWord();
+document.addEventListener('keypress', handleInput);
